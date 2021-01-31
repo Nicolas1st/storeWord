4ba5776f8d8d4dd0d5c0aac861fa825e84a823b2
Nicolas1st <nicolashereigo@gmail.com>
2021-01-31 21:16:52 +0300

    Made the navbar and the login and signup forms responsive

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+let sandwichButton = document.querySelector(".navbar__button")
+let links = document.querySelector(".navbar__links")
+sandwichButton.addEventListener("click", () => {
+    sandwichButton.classList.toggle("navbar__button_open");
+    if (links.style.display === "flex") {
+        links.style.display = "none";
+    } else {
+        links.style.display = "flex";
+    }
+});
